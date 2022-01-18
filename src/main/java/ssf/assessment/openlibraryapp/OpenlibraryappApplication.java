ac28b23f025c5198e78c0ac2667af706625cde0c
Shiraj <muhamadshiraj@gmail.com>
2022-01-18 09:49:14 +0800

    initial commit

---
+++ src/main/java/ssf/assessment/openlibraryapp/OpenlibraryappApplication.java
@@ -0,0 +1,13 @@
+package ssf.assessment.openlibraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlibraryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenlibraryappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/assessment/openlibraryapp/OpenlibraryappApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.assessment.openlibraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenlibraryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
